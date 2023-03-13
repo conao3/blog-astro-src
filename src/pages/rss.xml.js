5de6c43baaff44ea3e75f63a2403f06ac64eaaf2
Naoya Yamashita <conao3@gmail.com>
2023-03-14 04:32:01 +0900

    pass description

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,13 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    site: context.site,
+    description: '',
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }
