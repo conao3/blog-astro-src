79d692e16a90e70933ab09bf7345cdfb236f5585
Naoya Yamashita <conao3@gmail.com>
2024-08-04 03:42:53 +0900

    add expressiveCode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 import react from "@astrojs/react";
+import expressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://a.conao3.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [expressiveCode(), mdx(), sitemap(), react()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      theme: 'github-dark',
+    }
   }
 });
